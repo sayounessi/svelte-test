e19ac7470ab98d0e04682c9facfb5f0c37474441
sayounessi <nikitinjenya11@gmail.com>
2024-03-07 08:57:55 +0300

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Никитин Евгений</a>
         <div class="nav-links">
-           {#each nav as link}
+           {#if link.isShow}
            <a href={link.href} class="link">{link.title}</a>
-           {/each}
+           {/if}
         </div>
     </div>
 </nav>
@@ -18,23 +18,28 @@
     const nav = [
         {
             title:'Обо мне',
-            href:'/about'
+            href:'/about',
+            isShow: true,
         },
         {
             title:'Блог',
-            href:'/blog'
+            href:'/blog',
+            isShow: true,
         },
         {
             title:'Портфолио',
-            href:'/project'
+            href:'/project',
+            isShow: true,
         },
         {
             title:'Контакты',
-            href:'/contact'
+            href:'/contact',
+            isShow: true,
         },
         {
             title:'temp',
-            href:'/temp'
+            href:'/temp',
+            isShow: false,
         }
     ]
 </script>
